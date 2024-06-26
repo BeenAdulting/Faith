624266e12871136579442ac74ce7c75536462aa9
Xirfull <157926852+Xirfull@users.noreply.github.com>
2024-06-26 16:08:28 -0500

    Update Table.lua

--- Table.lua
+++ Table.lua
@@ -98,5 +98,5 @@ getgenv().Faith = {
     }
 }
 
-script_key = getgenv().Faith.key --// do not fucking touch this
+script_key = getgenv().Faith.Settings.Key --// do not fucking touch this
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/25c24c193c139ebcb35334998747d972.lua"))()
