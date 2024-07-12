804149f8662c044ecac3f5ca6e92d8a7b44a6583
Adulting <157926852+BeenAdulting@users.noreply.github.com>
2024-07-12 02:55:35 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -9,7 +9,7 @@ BronxSafteyCheck = {
 if BronxSafteyCheck.GameID == 17322605921 and BronxSafteyCheck.GameVersion == 2029 then  --// Bronx Streetz 2
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/4364901f24b8775e99ce13eaa93ec987.lua"))()
 elseif BronxSafteyCheck.GameID ~= 17322605921 and BronxSafteyCheck.GameVersion ~= 2029 then
-    game.Players.LocalPlayer:Kick("Faith Does Not Support this game")
+    game.Players.LocalPlayer:Kick("This game is not support by faith.")
 elseif BronxSafteyCheck.GameVersion == nil and BronxSafteyCheck.GameID == 17322605921 then
     game.Players.LocalPlayer:Kick("Game updated and Faith hasn't. Please wait for an update. Current PlaceVersion: " .. game.PlaceVersion)
 end
