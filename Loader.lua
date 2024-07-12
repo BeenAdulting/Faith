edb5f47aa05ab285aa8811be36e5ef240d869635
Adulting <157926852+BeenAdulting@users.noreply.github.com>
2024-07-12 02:47:29 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,15 +1,15 @@
 writefile("FaithKey.txt", script_key)
 script_key = readfile("FaithKey.txt")
 
-SafteyCheck = {
+BronxSafteyCheck = {
     GameVersion = game.PlaceVersion, 
     GameID = game.PlaceId,
 }
 
-if SafteyCheck.GameID == 17322605921 and SafteyCheck.GameVersion == 2029 then  --// Bronx
+if BronxSafteyCheck.GameID == 17322605921 and BronxSafteyCheck.GameVersion == 2029 then  --// Bronx Streetz 2
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/4364901f24b8775e99ce13eaa93ec987.lua"))()
-elseif SafteyCheck.GameID ~= 17322605921 and SafteyCheck.GameVersion ~= 2029 then
+elseif BronxSafteyCheck.GameID ~= 17322605921 and BronxSafteyCheck.GameVersion ~= 2029 then
     game.Players.LocalPlayer:Kick("Faith Does Not Support this game")
-elseif SafteyCheck.GameVersion == nil and SafteyCheck.GameID == 17322605921 then
+elseif BronxSafteyCheck.GameVersion == nil and BronxSafteyCheck.GameID == 17322605921 then
     game.Players.LocalPlayer:Kick("Game updated and Faith hasn't. Please wait for an update. Current PlaceVersion: " .. game.PlaceVersion)
 end
